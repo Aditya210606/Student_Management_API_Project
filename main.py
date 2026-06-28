7d1ec5129932e24ef62b362ec4338889b40a6431
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-06-28 20:21:08 +0530

    changes in the main file middlware registered

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ from fastapi.responses import JSONResponse
 from models import Student,UpdateStudent,StudentResponse,StudentSearch
 from data import load_data, save_data
 from routers.student import router
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.exceptions import RequestValidationError
-from exceptions.handlers import http_exception_handler,request_validatorError_handler
+from exceptions.handlers import http_exception_handler,request_validatorError_handler,generic_exception_handler
+from middleware.logging import log_request
+import time
 
 app =FastAPI()
 
+# includes all the routes from the router file in the app 
 app.include_router(router)
-  
 
-
+# this are the methods to register the expections   
 app.add_exception_handler( HTTPException, http_exception_handler)
 
 app.add_exception_handler(RequestValidationError,request_validatorError_handler)
+
+app.add_exception_handler(Exception, generic_exception_handler)
+
+app.middleware('http')(log_request)
+
+
+
 
 
 
