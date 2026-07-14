3ac2e1fc169981ad03f194518d6c26ee9e5fe8d6
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-07-14 20:27:49 +0530

    changes

--- routers/auth.py
+++ routers/auth.py
@@ -4,11 +4,23 @@ from sqlalchemy.orm import Session
 from database.session import get_db
 from schemas.auth import StudentLogin
 from services.auth_service import student_login_service
+from dependencies.auth import get_current_student
+from models.student import Student as StudentModel
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter(prefix="/auth",tags=["Authentication"])
 
 
 @router.post("/student/login")
-def student_login( login_data: StudentLogin, db: Session = Depends(get_db)):
-    return student_login_service(login_data, db)
+def student_login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    return student_login_service(form_data, db)
+
+@router.get("/student/me")
+def get_student_profile(current_student: StudentModel = Depends(get_current_student)):
+    return {
+        "student_id": current_student.student_id,
+        "first_name": current_student.first_name,
+        "last_name": current_student.last_name,
+        "email": current_student.email
+    }
