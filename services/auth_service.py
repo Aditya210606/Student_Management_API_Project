dc18be0533d54575e3d194ed8e4b01d50aa89cb5
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-07-14 20:26:53 +0530

    changes

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,49 +1,26 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
-from models.student import Student
+from models.student import Student as StudentModel
 from schemas.auth import StudentLogin
 from core.security import verify_password, create_access_token
 
 
-def student_login_service(login_data: StudentLogin, db: Session):
+def student_login_service(form_data, db: Session):
 
-    # Step 1: Find student in database
-    student = (
-        db.query(Student)
-        .filter(Student.student_id == login_data.student_id)
-        .first()
-    )
+    student = db.query(StudentModel).filter(
+        StudentModel.email == form_data.username
+    ).first()
 
-    # Step 2: Check whether student exists
     if not student:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid student ID or password"
-        )
+        raise HTTPException( status_code=401,detail="Invalid credentials" )
 
-    # Step 3: Verify entered password against stored hash
-    if not verify_password(
-        login_data.password,
-        student.password_hash
-    ):
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid student ID or password"
-        )
+    if not verify_password(form_data.password,student.password_hash):
+        raise HTTPException( status_code=401, detail="Invalid credentials")
 
-    # Step 4: Create JWT access token
-    access_token = create_access_token(
-        data={
-            "sub": student.student_id
-        }
-    )
+    access_token = create_access_token( {"sub": student.student_id,"role":"student"})
 
-    # Step 5: Return token
     return {
-    "message": "Login successful",
-    "access_token": access_token,
-    "token_type": "bearer",
-    "student_id": student.student_id,
-    "email": student.email
-}
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
