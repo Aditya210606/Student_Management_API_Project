601ce5a7252725403bbeba9336865a456733fcd0
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-07-24 10:52:24 +0530

    changes in the file

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -10,11 +10,7 @@ from models.student import Student as StudentModel
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
 
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid authentication credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials", headers={"WWW-Authenticate": "Bearer"})
 
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
@@ -27,14 +23,9 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     except JWTError:
         raise credentials_exception
 
-    student = db.query(StudentModel).filter(
-        StudentModel.student_id == student_id
-    ).first()
+    student = db.query(StudentModel).filter(StudentModel.student_id == student_id).first()
 
     if student is None:
         raise credentials_exception
 
-    return {
-        "user": student,
-        "role": role
-    }
+    return {"user": student, "role": role}
