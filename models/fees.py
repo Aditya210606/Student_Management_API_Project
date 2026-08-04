6c8ac00db4d75253cfac736ba779d1f215f124c0
Aditya Manoj Patil <Adityapatil210606@gmail.com>
2026-08-04 23:12:44 +0530

    changes

--- models/fees.py
+++ models/fees.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Mapped, mapped_column,relationship
 from datetime import datetime, date
 from database.database import Base
 
-
 class FeesModel(Base):
 
     __tablename__ = "fees"
